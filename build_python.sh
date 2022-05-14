97d2f6d762b2469ecbab399e8cd2e3355144542d
pandazxx <pandazxx@gmail.com>
2022-05-14 07:38:33 +0000

    First commit

---
+++ build_python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p ~/src ~/build/Python-3.10.4
+
+cd ~/src
+
+wget -c https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz
+
+tar xvf Python-3.10.4.tgz
+
+cd ~/build/Python-3.10.4
+
+~/src/Python-3.10.4/configure --perfix=$HOME/local
+
+time make
